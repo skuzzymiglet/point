449eed4f613a47f8e1f6f010d03e8df4681817e5
skuzzymiglet <skuzzymiglet@gmail.com>
2020-01-29 19:37:23 +0000

    script linting

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-var len;
-var slides;
-var index = 0;
-document.addEventListener('keydown', function(e){
-  slides = document.getElementsByClassName('slide');
-  len = slides.length;
+var len
+var slides
+var index = 0
+document.addEventListener('keydown', function (e) {
+  slides = document.getElementsByClassName('slide')
+  len = slides.length
   if (e.keyCode === 37) {
     index--
   } else if (e.keyCode === 39) {
     index++
   }
-  index %= len;
-  if (index < 0) {index = 0}
+  index %= len
+  if (index < 0) { index = 0 }
   Array.from(slides).forEach(function (e, i) {
-    if (i == index){
+    if (i === index) {
       e.style.display = 'block'
     } else {
       e.style.display = 'none'
     }
   })
 })
-
